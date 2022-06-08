a5a864947e3ca95e13feb4f184a6609f2fb9f821
barreirabruno <brunobarreirasilva@gmail.com>
2022-06-07 23:21:18 -0300

    chore: add SpringBoot config

---
+++ src/main/java/br/com/appjuneb/JuneStudyB/SpringBootConfig.java
@@ -0,0 +1,18 @@
+package br.com.appjuneb.JuneStudyB;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringBootConfig {
+
+    @Bean
+    public TransactionRepository database() {
+        return new TransactionRepository();
+    }
+
+    @Bean
+    public CreateTransactionUsecase create() {
+        return new CreateTransactionUsecase(this.database());
+    }
+}
