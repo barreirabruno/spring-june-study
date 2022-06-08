2f9414bf78d0d6f7082de3ffa89a467fa003bc06
barreirabruno <brunobarreirasilva@gmail.com>
2022-06-07 23:13:05 -0300

    feat: implements create method repository

--- src/main/java/br/com/appjuneb/JuneStudyB/TransactionRepository.java
+++ src/main/java/br/com/appjuneb/JuneStudyB/TransactionRepository.java
@@ -3,7 +3,13 @@ package br.com.appjuneb.JuneStudyB;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TransactionRepository {
+public class TransactionRepository implements TransactionRepositoryInterface {
 
     final private Map<String, Transaction> inMemoryDatabase = new HashMap<>();
+
+    @Override
+    public Transaction create(Transaction transaction) {
+        this.inMemoryDatabase.put(transaction.getId(), transaction);
+        return transaction;
+    }
 }
