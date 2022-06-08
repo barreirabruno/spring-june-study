555e8c154b38a6aa4bb87bf3bb0f083a3706c374
barreirabruno <brunobarreirasilva@gmail.com>
2022-06-07 23:40:08 -0300

    feat: implements findById route

--- src/main/java/br/com/appjuneb/JuneStudyB/TransactionController.java
+++ src/main/java/br/com/appjuneb/JuneStudyB/TransactionController.java
@@ -8,10 +8,17 @@ public class TransactionController {
 
     @Autowired
     private CreateTransactionUsecase createTransactionUsecase;
+    private FindTransactionUsecase findTransactionUsecase;
 
-    @RequestMapping(value="/", method= RequestMethod.GET)
+    @RequestMapping(value="/", method= RequestMethod.POST)
     @ResponseBody
     public Transaction create(@RequestBody Transaction transaction) {
         return this.createTransactionUsecase.create(transaction);
+    }
+
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    @ResponseBody
+    public Transaction findById(@PathVariable("transactionId") String transactionId) {
+        return this.findTransactionUsecase.findById(transactionId);
     }
 }
